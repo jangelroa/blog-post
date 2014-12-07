08673ebac589538c222199e441e0c96a14fb049c
jangelroa <jangelroa@gmail.com>
2014-12-07 14:09:31 -0800

    Created blog-post API, not created the DB yet

---
+++ db/seeds.rb
@@ -0,0 +1,32 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create([
+    { firstname: "Pepa", lastname: "Smith", email: "psmith@gmail.com", password: "p" },
+    { firstname: "Thalia", lastname: "Worries", email: "tworries@gmail.com", password: "t" },
+  ])
+
+Posts.create([
+    { title: "Problems in BART", body: "I think there are problems in BART" },
+    { title: "I love my cat", body: "I would die without my cat" }
+  ])
+
+Comment.create([
+    { body: "I don't think there are any problems in BART" },
+    { body: "I love my cat too, they are so adorable" }
+  ])
+
+Post.first.comments << Comment.first
+Post.last.comments << Comment.last
+
+User.first.posts << Post.first
+User.last.posts << Post.last
+
+
+
+
